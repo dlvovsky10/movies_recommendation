7d3329c5904d2ed7ac80323c6d13173cee3acbbf
Daniel <dlvovsky.10@gmail.com>
2025-07-15 13:21:18 +0300

    Add function that adds a genre to each movie

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import List, Dict, Tuple, Optional
 from colorama import init, Fore, Style
 import pandas as pd
 
+init()
+
+
 def get_movies_from_user_from_user() -> List[str]:
     movies = []
     movie_name = input("Please enter a movie name and enter quit to finish: ")
@@ -9,6 +12,7 @@ def get_movies_from_user_from_user() -> List[str]:
         movies.append(movie_name)
         movie_name = input("Please enter a movie name and enter quit to finish: ")
     return movies
+
 
 def rank_movies_with_user(movies: List[str]) -> Dict[str, int]:
     """
@@ -29,6 +33,19 @@ def rank_movies_with_user(movies: List[str]) -> Dict[str, int]:
             ranked_movies[movie] = rank
     return ranked_movies
 
+
+def add_genres_with_user(movies_df: pd.DataFrame):
+    """
+    Get Genre for each movie from the user
+    :param movies_df: Dataframe with movies details
+    """
+    genres = []
+    for index, row in movies_df.iterrows():
+        genre = input(f"Please enter the genre of the movie {row['name']}: ")
+        genres.append(genre)
+    movies_df['genre'] = genres
+
+
 def movies_data_frame_transformation(
         movies_df: Optional[pd.DataFrame] = None,
         movies_dict: Optional[Dict] = None):
@@ -44,12 +61,14 @@ def movies_data_frame_transformation(
     else:
         return movies_df
 
+
 def get_best_movie(
         movies_df: Optional[pd.DataFrame] = None,
         movies_dict: Optional[Dict] = None):
     movies_df = movies_data_frame_transformation(movies_df, movies_dict)
     temp_df = movies_df.sort_values(by='rank', ascending=False)
     return f"The best movie is {temp_df.iloc[0]['name']} with the rank of {temp_df.iloc[0]['rank']}"
+
 
 def get_worst_movie(
         movies_df: Optional[pd.DataFrame] = None,
@@ -58,11 +77,13 @@ def get_worst_movie(
     temp_df = movies_df.sort_values(by='rank', ascending=True)
     return f"The worst movie is {temp_df.iloc[0]['name']} with the rank of {temp_df.iloc[0]['rank']}"
 
+
 def get_avg_ranking(
         movies_df: Optional[pd.DataFrame] = None,
         movies_dict: Optional[Dict] = None):
     movies_df = movies_data_frame_transformation(movies_df, movies_dict)
-    return f"The average rank of the movies is {movies_df['rank'].mean()}"
+    avg = round(movies_df['rank'].mean(), 2)
+    return f"The average rank of the movies is {avg}"
 
 
 def main():
@@ -73,13 +94,16 @@ def main():
     # 2
     ranked_movies = rank_movies_with_user(movies_list)
     # 3
-    best_movie = get_best_movie(movies_dict=ranked_movies)
-    worst_movie = get_worst_movie(movies_dict=ranked_movies)
-    avg_ranking = get_avg_ranking(movies_dict=ranked_movies)
+    movies_df = movies_data_frame_transformation(movies_dict=ranked_movies)
+    best_movie = get_best_movie(movies_df)
+    worst_movie = get_worst_movie(movies_df)
+    avg_ranking = get_avg_ranking(movies_df)
     print(Fore.GREEN + best_movie + Style.RESET_ALL)
     print(Fore.RED + worst_movie + Style.RESET_ALL)
     print(Fore.YELLOW + avg_ranking + Style.RESET_ALL)
     # 4
+    add_genres_with_user(movies_df)
+    
 
 if __name__ == "__main__":
     main()
